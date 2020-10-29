37401ab79eaf4f0c2a19a63e1350de5594ab921e
GouthamGeek <Goutham.pga@gmail.com>
2020-10-29 18:31:42 -0400

    Midterm Completed

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://Goutham:e2BCE99QWyQ38QMK@cluster0.lxcxp.mongodb.net/Goutham?retryWrites=true&w=majority"
+};
